2f2765c3a9f23e0b430457b22d7c4c89c9cc90e3
HackBugs <shahnwaza7@gmail.com>
2024-12-24 17:34:32 +0530

    EDB Tools

---
+++ EDB-Tools/Script make multiple files.sh
@@ -0,0 +1,24 @@
+touch "EDB Backup and Recovery Tool.md" \
+touch "EDB Failover Manager.md" \
+touch "EDB LDAP Sync.md" \
+touch "EDB LiveCompare.md" \
+touch "EDB Migration Toolkit.md" \
+touch "EDB Postgres Enterprise Manager.md" \
+touch "EDB Postgres Extended Server.md" \
+touch "EDB Postgres for Kubernetes Operator.md" \
+touch "EDB Replication Server.md" \
+touch "EDB Postgres Advanced Server.md" \
+touch "Postgres Advanced Server Container Image.md" \
+touch "EDB PgBouncer.md" \
+touch "EDB Pgpool.md" \
+touch "EDB Language Pack.md" \
+touch "EDB Slony.md" \
+touch "EDB*Plus.md" \
+touch "EDB .net Connector.md" \
+touch "EDB JDBC Connector.md" \
+touch "EDB ODBC Connector.md" \
+touch "EDB Postgres Workload Reports (PWR).md" \
+touch "EDB OCL Connector.md" \
+touch "EDB StackBuilder Plus.md" \
+touch "EDB Postgres Distributed.md" \
+touch "EDB High Availability Routing for Postgres.md"
